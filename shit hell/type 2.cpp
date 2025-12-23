6b2589204432ba82893cf2f70af0a70dfe844fa5
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-12-23 23:05:46 +0600

    Add initial C++ file with basic setup

---
+++ shit hell/type 2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define     __        ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define     pi        acos(-1)
+#define     ll        long long int
+#define     fopen     freopen("input.txt","r",stdin)
+#define     fwrite    freopen("output.txt","w",stdout)
+#define     f0(n)     for(int i=0;i<n;i++)
+#define     mem(a)    memset(a,0,sizeof(a))
+#define     mems(a)   memset(a,-1,sizeof(a))
+int main (){
+  return 0;
+}
