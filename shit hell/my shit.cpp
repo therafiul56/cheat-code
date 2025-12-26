6fd8f53bd981b628fea57f42e44964d110d44bbd
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-12-26 19:54:03 +0600

    Add initial implementation of my shit.cpp

---
+++ shit hell/my shit.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+#define FastIo  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define sfn     scanf("%d",&n)
+#define ll  long long
+#define f0(n) for(int i = 0 ; i<n ; i++)
+#define f(n) for(int i = 1 ; i<=n ; i++)
